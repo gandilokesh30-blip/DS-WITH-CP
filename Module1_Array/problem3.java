5d7e02da9d4cd59ecef9d751bcebb6933779e7ae
gandilokesh30-blip <gandilokesh30@gmail.com>
2025-12-23 11:02:03 +0530

    Refactor array initialization and append logic
    Refactor array handling and append operation in main method.

--- Module1_Array/problem3.java
+++ Module1_Array/problem3.java
@@ -1,5 +1,4 @@
-class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         int[] n=new int[10];
         int cz=0;
         for(int i=0;i<5;i++){
@@ -8,8 +7,17 @@ class Main {
         cz=5;
         System.out.println(n.length);
      System.out.println(" currect size "+ cz);
-     for(int i=0;i<cz;i++){
+     for(int i=0;i<n.length;i++){
             System.out.println(i+" index "+n[i]);
         }
-    }
+    // oparation -1 Append 
+    int apVal=100;
+    n[cz]=apVal;
+    cz++;
+    System.out.println("after append oparation");
+    System.out.println(" currect size "+ cz);
+     for(int i=0;i<n.length;i++){
+            System.out.println(i+" index "+n[i]);
+        }
+}
 }
