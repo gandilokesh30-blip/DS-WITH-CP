dd013ae515494c56ea569435aaea74b74013788a
gandilokesh30-blip <gandilokesh30@gmail.com>
2025-12-23 09:43:55 +0530

    Rename class Task2 to problem2

--- Module1_Array/problem2.java
+++ Module1_Array/problem2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task2 {
+public class problem2 {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int[] array= new int[10];
